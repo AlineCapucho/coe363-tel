b056780e32ad38be11aa9716409cdba53a15dca5
aline.capucho <aline.capucho@olxbr.com>
2023-11-13 00:31:18 -0300

    feat: add more test data and use a loop to compress files

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 from huffman import huffman_compressor, huffman_decompressor
 from text_helpers import get_txt_size
 
-dracula = './raw-data/dracula.txt'
-dracula_compressed = './results/dracula-compressed.txt'
-dracula_decompressed = './results/dracula-decompressed.txt'
+books = [
+  {
+    'name': 'Dracula',
+    'original': './raw-data/dracula.txt',
+    'compressed':'./results/dracula-compressed.txt',
+    'decompressed': './results/dracula-decompressed.txt',
+  },
+  {
+    'name': 'Le Comte de Monte Cristo',
+    'original': './raw-data/le-comte-de-monte-cristo.txt',
+    'compressed':'./results/le-comte-de-monte-cristo-compressed.txt',
+    'decompressed': './results/le-comte-de-monte-cristo-decompressed.txt',
+  },
+  {
+    'name': 'Dom Casmurro',
+    'original': './raw-data/dom-casmurro.txt',
+    'compressed': './results/dom-casmurro-compressed.txt',
+    'decompressed': './results/dom-casmurro-decompressed.txt',
+  },
+  {
+    'name': 'La Divina Commedia',
+    'original': './raw-data/la-divina-commedia.txt',
+    'compressed': './results/la-divina-commedia-compressed.txt',
+    'decompressed': './results/la-divina-commedia-decompressed.txt',
+  },
+  {
+    'name': '入れかわった男',
+    'original': './raw-data/入れかわった男.txt',
+    'compressed': './results/入れかわった男-compressed.txt',
+    'decompressed': './results/入れかわった男-decompressed.txt',
+  },
+  {
+    'name': 'schumann',
+    'original': './raw-data/schumann.txt',
+    'compressed': './results/schumann-compressed.txt',
+    'decompressed': './results/schumann-decompressed.txt',
+  }
+]
 
-monteCristo = './raw-data/le-comte-de-monte-cristo.txt'
-monteCristo_compressed = './results/le-comte-de-monte-cristo-compressed.txt'
-monteCristo_decompressed = './results/le-comte-de-monte-cristo-decompressed.txt'
+for book in books:
+  huffman_compressor(book['original'], book['compressed'])
+  huffman_decompressor(book['original'], book['compressed'], book['decompressed'])
 
-domCasmurro = './raw-data/dom-casmurro.txt'
-domCasmurro_compressed = './results/dom-casmurro-compressed.txt'
-domCasmurro_decompressed = './results/dom-casmurro-decompressed.txt'
-
-huffman_compressor(dracula, dracula_compressed)
-huffman_decompressor(dracula, dracula_compressed, dracula_decompressed)
-
-huffman_compressor(monteCristo, monteCristo_compressed)
-huffman_decompressor(monteCristo, monteCristo_compressed, monteCristo_decompressed)
-
-huffman_compressor(domCasmurro, domCasmurro_compressed)
-huffman_decompressor(domCasmurro, domCasmurro_compressed, domCasmurro_decompressed)
-
-print('\nDRACULA')
-print('ORIGINAL - sizeof', get_txt_size(dracula))
-print('COMPRESSED - sizeof', get_txt_size(dracula_compressed))
-print('DECOMPRESSED - sizeof', get_txt_size(dracula_decompressed))
-
-print('\nMONTE CRISTO')
-print('ORIGINAL - sizeof', get_txt_size(monteCristo))
-print('COMPRESSED - sizeof', get_txt_size(monteCristo_compressed))
-print('DECOMPRESSED - sizeof', get_txt_size(monteCristo_decompressed))
-
-print('\nDOM CASMURRO')
-print('ORIGINAL - sizeof', get_txt_size(domCasmurro))
-print('COMPRESSED - sizeof', get_txt_size(domCasmurro_compressed))
-print('DECOMPRESSED - sizeof', get_txt_size(domCasmurro_decompressed))
+  print(f"\n{book['name']}")
+  print('ORIGINAL - sizeof', get_txt_size(book['original']))
+  print('COMPRESSED - sizeof', get_txt_size(book['compressed']))
+  print('DECOMPRESSED - sizeof', get_txt_size(book['decompressed']))
