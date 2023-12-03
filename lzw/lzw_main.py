ae60711e955371d2d73caac2650aab33cf41d2e1
Joao Pedro <jpmartinsf27@gmail.com>
2023-12-03 18:02:16 -0300

    Updated filepaths

--- lzw/lzw_main.py
+++ lzw/lzw_main.py
@@ -1,9 +1,9 @@
 from lzwTextHelpers import read_compressed_file, read_file, write_compressed_file, write_decompressed_file
 from lzw import LZWCompression
 
-input_filename = "raw-data/dracula.txt"
-compressed_filename = "results/dracula_lzw_encoded.txt"
-decompressed_filename = "results/dracula_lzw_decoded.txt"
+input_filename = "raw-data/le-comte-de-monte-cristo.txt"
+compressed_filename = "lzw_results/le-comte-de-monte-cristo_lzw_encoded.txt"
+decompressed_filename = "lzw_results/le-comte-de-monte-cristo_lzw_decoded.txt"
 compressor = LZWCompression()
 
 input_text = read_file(input_filename)
